7d3686277f1ebb74f628967181f943fc0fddd9cd
Abhi Kadam <kadamabhishek3188@gmail.com>
2025-01-02 23:25:05 +0530

    changes

--- app/Helpers/custom_helpers.php
+++ app/Helpers/custom_helpers.php
@@ -1,12 +1,39 @@
 <?php
 if (!function_exists('getMonth')) {
-    function getMonth($date) {
-        $months = array('January', 'February', 'March', 'April', 'May',
-        'June', 'July', 'August', 'September', 'October', 'November', 
-        'December');
+    function getMonth($date)
+    {
+        $months = array(
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+        );
         // return strtotime($date);
-        return $months[(date('n', strtotime($date))-1)];
+        return $months[(date('n', strtotime($date)) - 1)];
         // return $months[date('n') - 1];
+    }
+
+}
+
+if (!function_exists('pr')) {
+    function pr($data, $exit = true, $pre = true)
+    {
+
+        if ($pre !== false) {
+            echo "<pre>";
+        }
+        print_r($data);        
+        if ($exit !== false) {
+            exit;
+        }
     }
 
 }
